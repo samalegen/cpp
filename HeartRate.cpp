f9b094be14f381507563415a22f0bc17cf5c4d22
realmi_6s_termux <samalegen@gmail.com>
2021-02-15 22:35:36 +0300

    HartRate.cpp uograde

--- HeartRate.cpp
+++ HeartRate.cpp
@@ -10,8 +10,55 @@ HeartRates::HeartRates(string name, string surname, int day, int month, int year
     setMonth(month);
     setYear(year);
 }
-void HeartRates::setName(nam)
+void HeartRates::setName(string nam)
 {
     name = nam;
 }
+string HeartRates::getName()
+{
+    return name;
+}
+void HeartRates::setSurname(string surnam)
+{
+    surname = surnam;
+}
+string HeartRates::getSurname()
+{
+    return surname;
+}
+void HeartRates::setDay(int da)
+{
+    day = da;
+}
+int HeartRates::getDay()
+{
+    return day;
+}
+void HeartRates::setMonth(int mont)
+{
+    month = mont;
+}
+int HeartRates::getMonth()
+{
+    return month;
+}
+void HeartRates::setYear(int yea)
+{
+    year = yea;
+}
+int HeartRates::getYear()
+{
+    return year;
+}
+int HeartRates::getAge()
+{
+    return 2021-year;
+}
+int HeartRates::getMaxHartRate()
+{
+    return 220 - getMaxHartRate();
+}
+int HeartRates::getTargetHeartRate()
+{
 
+}
